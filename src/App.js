43e4debceeb99143ee1c5aed5af06b8068c7bc4b
KunalGoswami@Dell-7390 <kunalgoswami99@gmail.com>
2025-09-01 14:05:12 +0530

    Basic Changes - Changes in App File |

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <h1>My Todo List</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Task</th>
+                <th scope='col'>Assignee</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope='row'>1</td>
+                <td scope='row'>Learn React</td>
+                <td scope='row'>John Doe</td>
+              </tr>
+              <tr>
+                <td scope='row'>2</td>
+                <td scope='row'>Learn Java</td>
+                <td scope='row'>John Mark</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
     </div>
   );
 }
