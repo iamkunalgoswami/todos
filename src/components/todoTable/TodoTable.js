872e49efe5c4e0e38b0aa09376b110e085a76994
KunalGoswami@Dell-7390 <kunalgoswami99@gmail.com>
2025-09-07 13:54:50 +0530

    Rendering the list of todo table dynamically

--- src/components/todoTable/TodoTable.js
+++ src/components/todoTable/TodoTable.js
@@ -11,48 +11,14 @@ function TodoTable(props) {
                 </tr>
             </thead>
             <tbody>
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[0].id}
-                    description={props.TodoTableItem[0].description}
-                    assignee={props.TodoTableItem[0].assignee}
-                    isCompleted={props.TodoTableItem[0].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[1].id}
-                    description={props.TodoTableItem[1].description}
-                    assignee={props.TodoTableItem[1].assignee}
-                    isCompleted={props.TodoTableItem[1].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[2].id}
-                    description={props.TodoTableItem[2].description}
-                    assignee={props.TodoTableItem[2].assignee}
-                    isCompleted={props.TodoTableItem[2].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[3].id}
-                    description={props.TodoTableItem[3].description}
-                    assignee={props.TodoTableItem[3].assignee}
-                    isCompleted={props.TodoTableItem[3].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[4].id}
-                    description={props.TodoTableItem[4].description}
-                    assignee={props.TodoTableItem[4].assignee}
-                    isCompleted={props.TodoTableItem[4].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[5].id}
-                    description={props.TodoTableItem[5].description}
-                    assignee={props.TodoTableItem[5].assignee}
-                    isCompleted={props.TodoTableItem[5].isCompleted}
-                />
-                <TodoRowItem
-                    rowNumber={props.TodoTableItem[6].id}
-                    description={props.TodoTableItem[6].description}
-                    assignee={props.TodoTableItem[6].assignee}
-                    isCompleted={props.TodoTableItem[6].isCompleted}
-                />
+                {props.TodoTableItem.map(
+                    (todo) => <TodoRowItem
+                        rowNumber={todo.id}
+                        description={todo.description}
+                        assignee={todo.assignee}
+                        isCompleted={todo.isCompleted}
+                    />
+                )}
             </tbody>
         </table>
     )
